763b449204ab5f442b45df9b5a294a4cb9f4b0be
david.chen <david.chen@gigabyte.com>
2022-10-26 16:47:31 +0800

    格式化文件

--- QueueSender/Program.cs
+++ QueueSender/Program.cs
@@ -1,65 +1,63 @@
-﻿using Azure.Messaging.ServiceBus;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 
-namespace QueueSender
+namespace QueueSender;
+
+internal class Program
 {
-    internal class Program
+    private static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        // the client that owns the connection and can be used to create senders and receivers
+        ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        ServiceBusSender sender;
+
+        // number of messages to be sent to the queue
+        const int numOfMessages = 3;
+
+        // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when messages are being published or read
+        // regularly.
+        //
+        // set the transport type to AmqpWebSockets so that the ServiceBusClient uses the port 443. 
+        // If you use the default AmqpTcp, you will need to make sure that the ports 5671 and 5672 are open
+
+        // TODO: Replace the <NAMESPACE-CONNECTION-STRING> and <QUEUE-NAME> placeholders
+        var clientOptions = new ServiceBusClientOptions
         {
-            // the client that owns the connection and can be used to create senders and receivers
-            ServiceBusClient client;
+            TransportType = ServiceBusTransportType.AmqpWebSockets
+        };
+        client = new ServiceBusClient(
+            "Endpoint=sb://gbttest20221025.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=vwRSla2cZCncHqLPDzAN35xe0qhFqvSvUGaqo+7dNko=",
+            clientOptions);
+        sender = client.CreateSender("myqueue");
 
-            // the sender used to publish messages to the queue
-            ServiceBusSender sender;
+        // create a batch 
+        using var messageBatch = await sender.CreateMessageBatchAsync();
 
-            // number of messages to be sent to the queue
-            const int numOfMessages = 3;
+        for (var i = 1; i <= numOfMessages; i++)
+            // try adding a message to the batch
+            if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                // if it is too large for the batch
+                throw new Exception($"The message {i} is too large to fit in the batch.");
 
-            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
-            // of the application, which is best practice when messages are being published or read
-            // regularly.
-            //
-            // set the transport type to AmqpWebSockets so that the ServiceBusClient uses the port 443. 
-            // If you use the default AmqpTcp, you will need to make sure that the ports 5671 and 5672 are open
-
-            // TODO: Replace the <NAMESPACE-CONNECTION-STRING> and <QUEUE-NAME> placeholders
-            var clientOptions = new ServiceBusClientOptions
-            {
-                TransportType = ServiceBusTransportType.AmqpWebSockets
-            };
-            client = new ServiceBusClient("Endpoint=sb://gbttest20221025.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=vwRSla2cZCncHqLPDzAN35xe0qhFqvSvUGaqo+7dNko=", clientOptions);
-            sender = client.CreateSender("myqueue");
-
-            // create a batch 
-            using var messageBatch = await sender.CreateMessageBatchAsync();
-
-            for (var i = 1; i <= numOfMessages; i++)
-                // try adding a message to the batch
-                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
-                    // if it is too large for the batch
-                    throw new Exception($"The message {i} is too large to fit in the batch.");
-
-            try
-            {
-                // Use the producer client to send the batch of messages to the Service Bus queue
-                await sender.SendMessagesAsync(messageBatch);
-                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
-            }
-            finally
-            {
-                // Calling DisposeAsync on client types is required to ensure that network
-                // resources and other unmanaged objects are properly cleaned up.
-                await sender.DisposeAsync();
-                await client.DisposeAsync();
-            }
-
-            Console.WriteLine("Press any key to end the application");
-            Console.ReadKey();
+        try
+        {
+            // Use the producer client to send the batch of messages to the Service Bus queue
+            await sender.SendMessagesAsync(messageBatch);
+            Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
         }
+        finally
+        {
+            // Calling DisposeAsync on client types is required to ensure that network
+            // resources and other unmanaged objects are properly cleaned up.
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
+        Console.WriteLine("Press any key to end the application");
+        Console.ReadKey();
     }
 }
